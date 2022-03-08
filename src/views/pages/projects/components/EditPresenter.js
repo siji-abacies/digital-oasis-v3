2bdd2e3254c2d821feee6f6c58d0e3a42aa0f64a
siji-abacies <siji@abacies.com>
2022-03-08 16:33:34 +0530

    teleprompter

--- src/views/pages/projects/components/EditPresenter.js
+++ src/views/pages/projects/components/EditPresenter.js
@@ -225,7 +225,7 @@ import { useDispatch } from 'react-redux'
         hd_local_recording: hdRecord,
         send_email: sendMail, 
         custom_fields: customFields, 
-        is_password_protected: password_protected, 
+        is_password_protected: JSON.parse(data.password_protected), 
         password: data.password
       }
       console.log(d)
